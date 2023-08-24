b86e5db7d8ec2980ded33eb358896e848ac9ff29
vraciul52 <97583171+vraciul52@users.noreply.github.com>
2023-08-24 09:44:51 +0300

    Add files via upload

---
+++ budget.py
@@ -0,0 +1,72 @@
+class Category:
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        return False
+
+    def get_balance(self):
+        return sum(item["amount"] for item in self.ledger)
+
+    def transfer(self, amount, budget_category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {budget_category.category}")
+            budget_category.deposit(amount, f"Transfer from {self.category}")
+            return True
+        return False
+
+    def check_funds(self, amount):
+        return amount <= self.get_balance()
+
+    def __str__(self):
+        output = self.category.center(30, "*") + "\n"
+        for item in self.ledger:
+            desc = item["description"][:23]
+            amt = format(item["amount"], ".2f").rjust(30 - len(desc))
+            output += f"{desc}{amt}\n"
+        output += f"Total: {format(self.get_balance(), '.2f')}"
+        return output
+
+
+def create_spend_chart(categories):
+    spent_amounts = []
+    # Get total spent in each category
+    for category in categories:
+        spent = 0
+        for item in category.ledger:
+            if item["amount"] < 0:
+                spent += abs(item["amount"])
+        spent_amounts.append(round(spent, 2))
+
+    # Calculate percentage rounded down to the nearest 10
+    total = round(sum(spent_amounts), 2)
+    spent_percentage = list(map(lambda amount: int((((amount / total) * 10) // 1) * 10), spent_amounts))
+
+    # Create the bar chart substrings
+    header = "Percentage spent by category\n"
+
+    chart = ""
+    for value in reversed(range(0, 101, 10)):
+        chart += str(value).rjust(3) + '|'
+        for percent in spent_percentage:
+            if percent >= value:
+                chart += " o "
+            else:
+                chart += "   "
+        chart += " \n"
+
+    footer = "    " + "-" * ((3 * len(categories)) + 1) + "\n"
+    descriptions = list(map(lambda category: category.category, categories))  # Changed to category.category
+    max_length = max(map(lambda description: len(description), descriptions))
+    descriptions = list(map(lambda description: description.ljust(max_length), descriptions))
+    for x in zip(*descriptions):
+        footer += "    " + "".join(map(lambda s: s.center(3), x)) + " \n"
+
+    return (header + chart + footer).rstrip("\n")
